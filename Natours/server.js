f4209e043836ed34d3dfe6bbc4aeeb3d36619a55
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-01 00:50:56 +0530

    untracked the .env file for security reaasons.

--- Natours/server.js
+++ Natours/server.js
@@ -1,9 +1,40 @@
 const dotenv = require("dotenv");
-dotenv.config({ path: "./config.env" });
+// eslint-disable-next-line import/no-extraneous-dependencies
+const mongoose = require("mongoose");
 const app = require("./app");
 
-console.log(process.env);
+dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB)
+  // eslint-disable-next-line no-console
+  .then(() => console.log("DB connection succefull"));
+// console.log(process.env);
+
+const tourSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must contain a price"],
+  },
+});
+
+const Tour = mongoose.model("Tour", tourSchema);
 
 app.listen(process.env.PORT, () => {
+  // eslint-disable-next-line no-console
   console.log(`App running on port ${process.env.PORT}`);
 });
