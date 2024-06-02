61d56d7e61a5d6e27a1b2fcee3068eb18cc23cd8
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-02 21:55:04 +0530

    Added few queries to the api, one for sorting, selecting, fitlering and pagination. Shoudl add advanced query methods, one is to give aliases.

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -2,8 +2,55 @@ const Tour = require("../models/tourModel");
 
 const getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // bulid the query
+    // 1a) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ["page", "sort", "limit", "fields"];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 1b) Advanced Filtering
+    const queryStr = JSON.stringify(queryObj);
+    const queryStrReplaced = JSON.parse(
+      queryStr.replace(/\b(gte|gt|lt|lte)\b/g, (match) => `$${match}`)
+    );
+    // {difficulty : 'easy', duration : { $gte : 5}}
+
+    let query = Tour.find(queryStrReplaced);
+
+    // const query = await Tour.find()
+    //   .where("difficulty")
+    //   .equals("easy")
+    //   .where("duration")
+    //   .equals("5");
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sort = req.query.sort.split(",").join(" ");
+      query = query.sort(sort);
+    } // else query = query.sort("-createdAt");
+
+    // 3) selecting fields
+    if (req.query.fields) {
+      const fields = req.query.fields.split(",").join(" ");
+      query = query.select(fields);
+    } else query = query.select("-__v");
+
+    // 4) Limiting and pagination.
+    const limit = +req.query.limit || 100;
+    const page = +req.query.page || 1;
+    const skip = (page - 1) * limit;
+    query = query.skip(skip).limit(limit);
+
+    // Correctly check if the requested page exists
+    const totalPages = Math.ceil((await Tour.countDocuments()) / limit);
+    if (page > totalPages) {
+      throw new Error("This page does not exist");
+    }
+
+    // execute query
+    const tours = await query;
+
+    // send response
     res.status(200).json({
       status: "success",
       results: tours.length,
