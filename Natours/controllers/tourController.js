854e5a00dda1cabee4244fafa1af5c42bc922653
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-03 09:38:55 +0530

    Added aggreation pipeline inorder to solve bussiness startegic problems, and allowed an endpoint to get the stats of the database.

--- Natours/controllers/tourController.js
+++ Natours/controllers/tourController.js
@@ -111,3 +111,90 @@ const deleteTour = async (req, res) => {
   }
 };
 exports.deleteTour = deleteTour;
+
+// agerigation pipeline
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      { $match: { ratingsAverage: { $gte: 4.5 } } },
+      {
+        $group: {
+          _id: "$difficulty",
+          numTours: { $sum: 1 },
+          numsRating: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: {
+          avgPrice: 1,
+        },
+      },
+      // { $match: { _id: { $ne: "easy" } } },
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: { stats },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = +req.params.year;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: {
+          _id: 0,
+        },
+      },
+      {
+        $sort: {
+          numTourStarts: -1,
+        },
+      },
+      // {$limit: 12}
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      data: { plan },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
--- Natours/routes/tourRoute.js
+++ Natours/routes/tourRoute.js
@@ -6,12 +6,16 @@ const {
   getTour,
   updateTour,
   deleteTour,
+  getTourStats,
+  getMonthlyPlan,
 } = require("../controllers/tourController");
 
 const router = express.Router();
 
 // router.param("id", checkId);
 
+router.route("/monthly-plan/:year").get(getMonthlyPlan);
+router.route("/tour-stats").get(getTourStats);
 router.route("/top-5-cheap").get(getAliasTop, getAllTours);
 router.route("/").get(getAllTours).post(createTour);
 router.route("/:id").get(getTour).patch(updateTour).delete(deleteTour);
