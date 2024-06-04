e082c96d1b3a52874a097aa250435918caecad7b
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-05 00:26:41 +0530

    Learnt how to send emails using nodemailer, scompleted one part of the reset password fucntionality.

--- Natours/models/userModel.js
+++ Natours/models/userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const crypto = require("crypto");
 const validator = require("validator");
 const bcrypt = require("bcryptjs");
 
@@ -40,6 +41,8 @@ const userSchema = mongoose.Schema({
     },
   },
   passwordChangedAt: Date,
+  passwordResetToken: String,
+  passwordResetTimeExpires: Date,
 });
 
 userSchema.pre("save", async function (next) {
@@ -69,6 +72,20 @@ userSchema.methods.changedPasswordAfter = function (JWTTimestamp) {
 
   // false means not changed
   return false;
+};
+
+userSchema.methods.createPasswordResetToken = function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto
+    .createHash("sha256")
+    .update(resetToken)
+    .digest("hex");
+  this.passwordResetTimeExpires = Date.now() + 10 * 60 * 1000;
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  return resetToken;
 };
 
 const User = mongoose.model("User", userSchema);
