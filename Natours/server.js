2002efade2660eff89ddc137ae3a1f6f7fb27cf4
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-04 10:51:25 +0530

    Done with the error handling in my api, handled uncaught and unhandled exception and rejections, should move on to advanced mongoose, auth and manymore.

--- Natours/server.js
+++ Natours/server.js
@@ -2,6 +2,17 @@ const dotenv = require("dotenv");
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require("mongoose");
 
+process.on("uncaughtException", (err) => {
+  // eslint-disable-next-line
+  console.log(
+    err.name,
+    err.message,
+    "\n",
+    "Uncaught exception! 💥 Shutting down."
+  );
+  process.exit(1);
+});
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
 
@@ -14,9 +25,23 @@ mongoose
   .connect(DB)
   // eslint-disable-next-line no-console
   .then(() => console.log("DB connection succefull"));
+// .catch(() => "hello");
 // console.log(process.env);
 
-app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App running on port ${process.env.PORT}`);
+});
+
+process.on("unhandledRejection", (err) => {
+  // eslint-disable-next-line
+  console.log(
+    err.name,
+    err.message,
+    "\n",
+    "Unhandled Rejection! 💥 Shutting down."
+  );
+  server.close(() => {
+    process.exit(1);
+  });
 });
