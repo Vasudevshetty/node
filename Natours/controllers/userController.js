26636ddc6cc4836e1f0e983f56e42942ed15731e
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-05 08:40:26 +0530

    Updating user data done.

--- Natours/controllers/userController.js
+++ Natours/controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require("../models/userModel");
+const AppError = require("../utils/appError");
 const { catchAsync } = require("../utils/catchAsync");
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
@@ -7,6 +16,29 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     status: "success",
     results: users.length,
     data: { users },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error is user post passwords data
+  if (req.body.password || req.body.passwordConfirm)
+    return next(
+      new AppError(
+        "This route is not for password updates, please use /updateMyPassword",
+        400
+      )
+    );
+
+  // 2) update user doc
+  const filteredBody = filterObj(req.body, "name", "email");
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: { user: updatedUser },
   });
 });
 
