d65902a6b3952c913adac63ef0ce82e0399e591a
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-06 21:41:44 +0530

    Done with the authenticatin and authorisation part, finshed with the security measures for the api built, security http header, body parser, sanitisation(noSQL, XSS) and paramater pollution.

--- Natours/app.js
+++ Natours/app.js
@@ -1,5 +1,11 @@
 const express = require("express");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const mongoSanitize = require("express-mongo-sanitize");
+const hpp = require("hpp");
+
 const tourRouter = require("./routes/tourRoute");
 const userRouter = require("./routes/userRoute");
 const AppError = require("./utils/appError");
@@ -7,16 +13,57 @@ const errorController = require("./controllers/errorController");
 
 const app = express();
 
+// set security http header
+app.use(helmet());
+
+// developement logging
 if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 
-app.use(express.json());
-// app.use(express.static(`${__dirname}/public`));
+// global middleware, limit requests from same ip
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: "Too many request from this IP, please try again in an hour!",
+});
+app.use("/api", limiter);
 
+// body parser, reading data
+app.use(
+  express.json({
+    limit: "10kb",
+  })
+);
+
+// data sanitaisation against noSQL injection
+app.use(mongoSanitize());
+
+// data sanitaisation against XSS
+app.use(xss());
+
+// prevent paramater pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+);
+
+// serving static files
+app.use(express.static(`${__dirname}/public`));
+
+// test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
 
+// Routes
 app.use("/api/v1/tours", tourRouter);
 app.use("/api/v1/users", userRouter);
 
