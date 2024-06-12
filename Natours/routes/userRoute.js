0abe1fb3509dab13dc846b14ace828296e7e8c6f
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-12 08:24:57 +0530

    added a get/me route  to get the user stats, and build up a me stats thorugh this endpoint.

--- Natours/routes/userRoute.js
+++ Natours/routes/userRoute.js
@@ -7,6 +7,7 @@ const {
   deleteUser,
   updateMe,
   deleteMe,
+  getMe,
 } = require("../controllers/userController");
 const {
   signup,
@@ -18,6 +19,8 @@ const {
 } = require("../controllers/authController");
 
 const router = express.Router();
+
+router.get("/me", protect, getMe, getUser);
 
 router.post("/signup", signup);
 router.post("/login", login);
