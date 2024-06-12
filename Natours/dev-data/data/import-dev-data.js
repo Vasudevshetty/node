e5bd8414b79f1f252278b8d0e232da7e3aad051b
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-12 09:01:17 +0530

    Updated importing and deleting dev-data script file to work with development phase and get the dev-data ready for serverside rendering.

--- Natours/dev-data/data/import-dev-data.js
+++ Natours/dev-data/data/import-dev-data.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const Tour = require("../../models/tourModel");
+const User = require("../../models/userModel");
+const Review = require("../../models/reviewModel");
 
 dotenv.config({ path: "./config.env" });
 
@@ -14,10 +16,16 @@ const DB = process.env.DATABASE.replace(
 mongoose.connect(DB).then(() => console.log("DB connection succefull"));
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/users.json`, "utf-8"));
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/reviews.json`, "utf-8")
+);
 
 const importData = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, { validateBeforeSave: false });
+    await Review.create(reviews);
     console.log("Data Successfully loaded !");
   } catch (error) {
     console.log(error);
@@ -29,6 +37,8 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log("Data succesfully deleted.");
   } catch (error) {
     console.log(error);
