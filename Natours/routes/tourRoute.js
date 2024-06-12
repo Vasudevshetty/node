3d78de27c4c3e4cf9c06805f6266de2066dd60f0
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-12 08:50:34 +0530

    Done with the missing authentication and authorization in the api.

--- Natours/routes/tourRoute.js
+++ Natours/routes/tourRoute.js
@@ -21,15 +21,20 @@ router.use("/:tourId/reviews", reviewRouter);
 
 // router.param("id", checkId);
 
-router.route("/monthly-plan/:year").get(getMonthlyPlan);
 router.route("/tour-stats").get(getTourStats);
 router.route("/top-5-cheap").get(getAliasTop, getAllTours);
+router
+  .route("/monthly-plan/:year")
+  .get(protect, restrictTo("admin", "lead-guide", "guide"), getMonthlyPlan);
 
-router.route("/").get(protect, getAllTours).post(createTour);
+router
+  .route("/")
+  .get(getAllTours)
+  .post(protect, restrictTo("admin", "lead-guide"), createTour);
 router
   .route("/:id")
   .get(getTour)
-  .patch(updateTour)
+  .patch(protect, restrictTo("admin", "lead-guide"), updateTour)
   .delete(protect, restrictTo("admin", "lead-guide"), deleteTour);
 
 module.exports = router;
--- Natours/routes/userRoute.js
+++ Natours/routes/userRoute.js
@@ -16,22 +16,24 @@ const {
   resetPassword,
   updatePassword,
   protect,
+  restrictTo,
 } = require("../controllers/authController");
 
 const router = express.Router();
 
-router.get("/me", protect, getMe, getUser);
-
 router.post("/signup", signup);
 router.post("/login", login);
-
 router.post("/forgotPassword", forgotPassword);
 router.patch("/resetPassword/:token", resetPassword);
-router.patch("/updateMyPassword", protect, updatePassword);
 
-router.patch("/updateMe", protect, updateMe);
-router.delete("/deleteMe", protect, deleteMe);
+router.use(protect);
 
+router.patch("/updateMyPassword", updatePassword);
+router.get("/me", getMe, getUser);
+router.patch("/updateMe", updateMe);
+router.delete("/deleteMe", deleteMe);
+
+router.use(restrictTo("admin"));
 router.route("/").get(getAllUsers).post(createUser);
 router.route("/:id").get(getUser).patch(updateUser).delete(deleteUser);
 
