91002c1d7259455cf11be3ca5147402026852080
Vasudevshetty <vasudeepu2815@gmail.com>
2024-06-13 07:18:28 +0530

    Calculated average ratings and ratings quantity for every tour, on succesffuly crud operation on reviews.

--- Natours/models/reviewModel.js
+++ Natours/models/reviewModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Tour = require("./tourModel");
 
 const reviewSchema = mongoose.Schema(
   {
@@ -26,12 +27,8 @@ const reviewSchema = mongoose.Schema(
       required: [true, "Review must belong to a user"],
     },
   },
-  {
-    toJSON: { virtuals: true },
-    toObject: { virtuals: true },
-  }
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
-
 reviewSchema.pre(/^find/, function (next) {
   //   this.populate({
   // path: "tour",
@@ -42,6 +39,41 @@ reviewSchema.pre(/^find/, function (next) {
     select: "name",
   });
   next();
+});
+
+reviewSchema.statics.calcAverageRatings = async function (tourId) {
+  const stats = await this.aggregate([
+    {
+      $match: { tour: tourId },
+    },
+    {
+      $group: {
+        _id: "$tour",
+        nRating: { $sum: 1 },
+        avgRating: { $avg: "$rating" },
+      },
+    },
+  ]);
+
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingsQuantity: stats[0].nRating,
+      ratingsAverage: stats[0].avgRating,
+    });
+  }
+};
+
+reviewSchema.post("save", function () {
+  this.constructor.calcAverageRatings(this.tour);
+});
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.review = await this.findOne();
+  next();
+});
+
+reviewSchema.pre(/^findOneAnd/, async function () {
+  this.review.calcAverageRatings(this.review.tour);
 });
 
 const Review = mongoose.model("Review", reviewSchema);
